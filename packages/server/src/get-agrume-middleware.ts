d210a9ad3260df2c8bc563ce3e42f5f1f9519fb6
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-04-22 23:27:03 +0200

    Fix bad identification of external dependencies in Agrume CLI

--- packages/server/src/get-agrume-middleware.ts
+++ packages/server/src/get-agrume-middleware.ts
@@ -1,3 +1,5 @@
+import process from 'node:process'
+import path from 'node:path'
 import * as vite from 'vite'
 import agrumeVitePlugin from '@agrume/plugin/vite'
 import type Connect from 'connect'
@@ -30,7 +32,7 @@ export function getAgrumeMiddleware(
               return false
             }
 
-            return !source.includes('node_modules')
+            return path.relative(process.cwd(), source).startsWith('node_modules')
           },
         },
         write: false,
