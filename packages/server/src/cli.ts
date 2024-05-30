a22a091107d23774d52bd922a6d66ab9158ba1f1
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-05-30 23:06:51 +0200

    Add option on the CLI to watch files

--- packages/server/src/cli.ts
+++ packages/server/src/cli.ts
@@ -10,6 +10,7 @@ program
   .option('-p, --port <port>', 'Port to listen on', '3000')
   .option('-h, --host <host>', 'Host to listen on', 'localhost')
   .option('-e, --entry <entry>', 'The entry files to search for routes', 'index.js,index.ts,index.jsx,index.tsx,main.js,main.ts,main.jsx,main.tsx,app.js,app.ts,app.jsx,app.tsx,src/index.js,src/index.ts,src/index.jsx,src/index.tsx,src/main.js,src/main.ts,src/main.jsx,src/main.tsx,src/app.js,src/app.ts,src/app.jsx,src/app.tsx')
+  .option('--watch [target]', 'Watch for changes in the target directory')
   .option('--tunnel [tunnel]', 'Register a tunnel')
   .option('--allow-unsafe', 'Allow loading routes from node_modules')
   .action(async (options) => {
@@ -19,6 +20,7 @@ program
       host: options.host,
       port: Number.parseInt(options.port),
       tunnel: options.tunnel === true ? 'localtunnel' : options.tunnel,
+      watch: options.watch,
     })
   })
   .allowExcessArguments(false)
