1162bbd239d62f2b3502ceed9504685c687fdd89
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-08-29 21:54:13 +0200

    Test if passing parameters work

--- packages/agrume/tests-e2e/simplest-create-route.test.ts
+++ packages/agrume/tests-e2e/simplest-create-route.test.ts
@@ -27,4 +27,31 @@ describe('simplest usage of `createRoute`', () => {
     expect(response.statusCode).toBe(200)
     expect(response.json<string>()).toBe(EXPECTED_RETURN)
   })
+
+  it('should work with parameters', async () => {
+    const API_PATH = '/hello-param'
+    const PARAM_NAME = 'name'
+    const PARAM_VALUE = 'Planet'
+
+    const { close, server } = await loadAgrumeProject(/* tsx */`
+      import React from 'react'
+      import { createRoute } from 'agrume'
+      
+      const r = createRoute(async ({ ${PARAM_NAME} }) => 'Hello ' + ${PARAM_NAME}, { path: '${API_PATH}' })
+
+      export function App() {
+        return null
+      }
+    `)
+
+    const response = await inject(server, {
+      body: JSON.stringify({ [PARAM_NAME]: PARAM_VALUE }),
+      method: 'POST',
+      url: `/api${API_PATH}`,
+    })
+    await close()
+
+    expect(response.statusCode).toBe(200)
+    expect(response.json<string>()).toBe(`Hello ${PARAM_VALUE}`)
+  })
 })
