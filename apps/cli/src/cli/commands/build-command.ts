eea8cc305f4dd1d973a29c8e706c674f0458d020
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-11-01 12:33:31 +0100

    Replace 0 by empty string to not generate listen code

--- apps/cli/src/cli/commands/build-command.ts
+++ apps/cli/src/cli/commands/build-command.ts
@@ -16,7 +16,7 @@ export const buildCommand = createCommand('build')
   .option('-o, --output <output>', 'The output directory', 'build')
   .option('--disable-logger', 'Disable the logger')
   .option('--single-file', 'Generate a single file', false)
-  .option('-p, --port <port>', 'The port the server will listen on. By default, it uses the port from the config file. Set to 0 to not generate listen code. Set a string to select an environment variable.')
+  .option('-p, --port <port>', 'The port the server will listen on. By default, it uses the port from the config file. Set a string to select an environment variable. Set to an empty string to not generate listen code.')
   .option('-w, --watch [target]', 'Watch for changes in the target directory')
   .action(async (library, options) => {
     // This is a workaround to still get the options even if they are parsed in the parent command
@@ -49,7 +49,7 @@ export const buildCommand = createCommand('build')
       await runBuilder(builder, {
         destination: options.output,
         enableLogger: !options.disableLogger,
-        listen: options.port === '0' ? undefined
+        listen: options.port === '' ? undefined
         : (options.port ?? (config.port ? config.port.toString() : undefined)),
         singleFile: options.singleFile,
       })
